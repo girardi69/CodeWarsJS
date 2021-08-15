5e3ed79981086d0f0f4017e6d499e22f6b40159f
Andrea Girardi <girardi69@gmail.com>
2021-08-15 16:06:55 +0200

    Update exercises.ts

--- 7 kyu/exercises.ts
+++ 7 kyu/exercises.ts
@@ -5,7 +5,17 @@ function squareDigits(num){
     newArr.push(parseInt(arr[i]**2))
   };
   return Number(newArr.join(""));
-}  
-  
+} 
 const squareDigits = (num) => String(num).split("").map(c => c *c).join(""));  
   
+*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@
+
+function multiple(x) {
+  return x % 3 == 0 && x % 5 == 0 ? "BangBoom" :
+    x % 5 == 0 ? "Boom" :
+    x % 3 == 0 ? "Bang" : 
+    "Miss"
+}
+const multiple = x => x%15?x%5?x%3? 'Miss':'Bang':'Boom':'BangBoom'
+
+*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@
