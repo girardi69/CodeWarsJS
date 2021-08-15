c2029d5934e7af85390c2518419f5f8be1531f75
Andrea Girardi <girardi69@gmail.com>
2021-08-15 15:55:25 +0200

    Update exercises.ts

--- 7 kyu/exercises.ts
+++ 7 kyu/exercises.ts
@@ -1,2 +1,11 @@
-''' const squareDigits = (num) => String(num).split("").map(c => c *c).join("")); '''
-
+function squareDigits(num){
+  let arr = String(num).split("");
+  let newArr = [];
+  for ( i = 0; i < arr.length; i++){
+    newArr.push(parseInt(arr[i]**2))
+  };
+  return Number(newArr.join(""));
+}  
+  
+const squareDigits = (num) => String(num).split("").map(c => c *c).join(""));  
+  
